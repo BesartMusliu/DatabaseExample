6eac419f52b126829ad88e538dbae831baa5a823
Milot Shala <milot.shala@gmail.com>
2014-04-16 20:30:03 +0200

    Added multi-threading support

--- src/com/milot/databaseexample/MainActivity.java
+++ src/com/milot/databaseexample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.milot.databaseexample;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -11,10 +13,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	DocumentDbHandler db;
+	List<Document> documents = new ArrayList<Document>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +27,16 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		db = new DocumentDbHandler(this);	         
+        
+        db.addDocument(new Document("test.pdf", "/Downloads/Tmp"));        
+        db.addDocument(new Document("Invoice.doc", "/Documents/Invoices"));
+        db.addDocument(new Document("Thor 1080p.mkv", "/Movies"));
+        db.addDocument(new Document("Game of Thrones Season 4 Episode 2 720p.mp4", "/Movies"));
+        
+     
+        new AsyncKlasa().execute();
 	}
 
 	@Override
@@ -46,6 +59,43 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	
+	
+	public class AsyncKlasa extends AsyncTask</*doInBg*/ Void, /*progressUpdate*/ Integer, /*postExec*/ String> {
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			documents = db.getAllDocuments();
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {			
+			super.onPostExecute(result);
+			
+			for (Document doc : documents) {
+	            String document = "Id: "+doc.getID()+" ,Name: " + doc.getName() + " ,Phone: " + doc.getPath();
+	            Log.e("Document: ", document);
+	        }
+			
+			// p.sh. adapteri.notifyDataSetChanged();
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+		
+		
+	}
+	
+	
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
@@ -59,20 +109,6 @@ public class MainActivity extends ActionBarActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
-			
-			DocumentDbHandler db = new DocumentDbHandler(getActivity());	         
-	         
-	        db.addDocument(new Document("test.pdf", "/Downloads/Tmp"));        
-	        db.addDocument(new Document("Invoice.doc", "/Documents/Invoices"));
-	        db.addDocument(new Document("Thor 1080p.mkv", "/Movies"));
-	        db.addDocument(new Document("Game of Thrones Season 4 Episode 2 720p.mp4", "/Movies"));
-	          
-	        List<Document> documents = db.getAllDocuments();       
-	         
-	        for (Document doc : documents) {
-	            String document = "Id: "+doc.getID()+" ,Name: " + doc.getName() + " ,Phone: " + doc.getPath();
-	            Log.e("Document: ", document);
-	        }
 			return rootView;
 		}
 	}
